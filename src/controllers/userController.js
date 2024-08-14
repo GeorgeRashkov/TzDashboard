a35c04201de21dc43cb4d0f968d8ff785a18939f
George Rashkov <support@indago-dev.com>
2024-08-15 02:04:18 +0300

    server resolve

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -8,27 +8,33 @@ const cloudinary = require('cloudinary');
 
 // Register User
 exports.registerUser = asyncErrorHandler(async (req, res, next) => {
-
-    const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
-        folder: "avatars",
-        width: 150,
-        crop: "scale",
-    });
-
-    const { name, email, gender, password } = req.body;
-
-    const user = await User.create({
-        name, 
-        email,
-        gender,
-        password,
-        avatar: {
-            public_id: myCloud.public_id,
-            url: myCloud.secure_url,
-        },
-    });
-
-    sendToken(user, 201, res);
+    try{
+        const myCloud = await cloudinary.v2.uploader.upload(file=req.body.avatar, {
+            folder: "avatars",
+            width: 150,
+            crop: "scale",
+        });
+    
+        const { name, email, gender, password } = req.body;
+        const user = await User.create({
+            name, 
+            email,
+            gender,
+            password,
+            avatar: {
+                public_id: myCloud.public_id,
+                url: myCloud.secure_url,
+            },
+        });
+    
+        sendToken(user, 201, res);
+    } catch (err) {
+        process.stderr.write(err.message);
+        res.status(500).json({
+            success: false,
+            error: err.message,
+        });
+    }
 });
 
 // Login User
