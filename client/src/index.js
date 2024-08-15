49c4cb3188274b4c016df9735c1b506c6e50e860
unnv <nazik.klas.opaopaopapa@gmail.com>
2024-08-15 03:16:53 +0300

    fix

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store';
-import { SnackbarProvider } from 'notistack';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import { SnackbarProvider } from "notistack";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route
+} from "react-router-dom";
+import App from "./App";
+import Login from "./components/User/Login";
+import Register from "./components/User/Register";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route index element={<App />} />{" "}
+      <Route path="/login" element={<Login />} />{" "}
+      <Route path="Register" element={<Register />} />{" "}
+    </Route>
+  )
+);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,15 +30,13 @@ ReactDOM.render(
       <SnackbarProvider
         maxSnack={2}
         anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'center',
+          vertical: "bottom",
+          horizontal: "center"
         }}
       >
-        <Router>
-          <App />
-        </Router>
-      </SnackbarProvider>
-    </Provider>
+        <RouterProvider router={router} />{" "}
+      </SnackbarProvider>{" "}
+    </Provider>{" "}
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
